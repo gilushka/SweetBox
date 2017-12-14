1649b4f9e4112f09f276e39cd7edf8e7a63ff020
gilushka <EzhiKN@gmail.com>
2017-12-14 11:18:57 +0300

    Создал основные классы

---
+++ src/main/java/MakePresent.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 14.12.2017.
+ */
+public class MakePresent {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Sweets/Sweet.java
@@ -0,0 +1,13 @@
+package Sweets;
+
+/**
+ * Created by user on 14.12.2017.
+ */
+public abstract class Sweet {
+    private String sName;
+    private float fCost;
+    private int iWeight;
+    public Sweet() {
+    }
+
+}
