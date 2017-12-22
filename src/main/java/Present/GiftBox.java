dde56870a74e4cca17f4c1639efac6125460435f
gilushka <EzhiKN@gmail.com>
2017-12-23 00:15:01 +0300

    В GiftBox'е массив заменен на LinkedList.

--- src/main/java/Present/GiftBox.java
+++ src/main/java/Present/GiftBox.java
@@ -2,31 +2,35 @@ package Present;
 
 import Sweets.Sweet;
 
+import java.util.LinkedList;
+
 /**
  * Класс формирования подарка.
  */
 public class GiftBox {
     private float fTotalWeight = 0; // Общий вес
     private int iTotalCost = 0;  // Общая цена
-    Sweet[] swArray = new Sweet[0]; // Массив в котором хранятся вкусняшки
+//    Sweet[] swArray = new Sweet[0]; // Массив в котором хранятся вкусняшки
+    LinkedList<Sweet> list = new LinkedList<Sweet>();
 
     public void addItem(Sweet sweet){
-        int iLen = swArray.length; // Длинна существующего массива вкусняшек
+/*        int iLen = swArray.length; // Длинна существующего массива вкусняшек
         Sweet[] swNewArray = new Sweet[iLen+1]; // Новый массив на +1 количество сладостей
         for (int i = 0; i < iLen; i++) {
             swNewArray[i] = swArray[i]; // Копируем сладости в новый массив
         }
         swNewArray[iLen] = sweet; // Добавляем в конец новую сладость
-        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент
+        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент*/
         fTotalWeight += sweet.getfWeight();
         iTotalCost += sweet.getiCost();
+        list.add(sweet);
     }
 
     /**
      * Добавление сладости в середину по индексу
      */
     public void addItemInd(Sweet sweet, int iInd){
-        int iLen = swArray.length; // Длинна существующего массива вкусняшек
+/*        int iLen = swArray.length; // Длинна существующего массива вкусняшек
         Sweet[] swNewArray = new Sweet[iLen+1]; // Новый массив на +1 количество сладостей
         for (int i = 0; i < iInd-1; i++) {
             swNewArray[i] = swArray[i]; // Копируем сладости в новый массив
@@ -35,9 +39,10 @@ public class GiftBox {
         for (int i = iInd; i < iLen+1; i++) {
             swNewArray[i] = swArray[i-1]; // Копируем вторую часть старого массива в новый
         }
-        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент
+        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент*/
         fTotalWeight += sweet.getfWeight();
         iTotalCost += sweet.getiCost();
+        list.add(iInd-1, sweet);
     }
 
 
@@ -46,22 +51,23 @@ public class GiftBox {
      * Удаление сладости по индексу
      */
     public void delItemInd(int iInd){
-        int iLen = swArray.length; // Длинна существующего массива вкусняшек
-        fTotalWeight -= swArray[iInd-1].getfWeight();
-        iTotalCost -= swArray[iInd-1].getiCost();
+//        int iLen = swArray.length; // Длинна существующего массива вкусняшек
+        fTotalWeight -= list.get(iInd-1).getfWeight();
+        iTotalCost -= list.get(iInd-1).getiCost();
 
-        Sweet[] swNewArray = new Sweet[iLen-1]; // Новый массив на +1 количество сладостей
+/*        Sweet[] swNewArray = new Sweet[iLen-1]; // Новый массив на +1 количество сладостей
         for (int i = 0; i < iInd-1; i++) {
             swNewArray[i] = swArray[i]; // Копируем сладости в новый массив
         }
         for (int i = iInd-1; i < iLen-1; i++) {
             swNewArray[i] = swArray[i+1]; // Копируем сладости в новый массив
         }
-        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент
+        swArray = swNewArray; // Присваиваем переменной swArray ссылку на новый созданный массив длинной +1 элемент*/
+        list.remove(iInd-1);
     }
 
     public void printItems(){
-        for (Sweet aSwArray : swArray) {
+        for (Sweet aSwArray : list) {
             System.out.println(aSwArray.description());
         }
     }
@@ -75,14 +81,14 @@ public class GiftBox {
     }
 
     public int getiItemCount(){
-        return swArray.length;
+        return list.size();
     }
 
-    public Sweet[] getSwArray() {
-        return swArray;
+    public LinkedList<Sweet> getSwArray() {
+        return list;
     }
 
     public int getSwArrayElementCost(int i) {
-        return swArray[i-1].getiCost();
+        return list.get(i-1).getiCost();
     }
 }
